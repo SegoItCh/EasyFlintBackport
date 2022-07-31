195f2c186210a53ab40c4a22277e393f7b4a8bfb
Jan Grigat <j.grigat+github@cosmos-ink.net>
2022-07-31 07:55:39 +0000

    added sound, fixed inventory updating

--- src/main/java/com/myname/mymodid/GravelEvent.java
+++ src/main/java/com/myname/mymodid/GravelEvent.java
@@ -3,7 +3,9 @@ package com.myname.mymodid;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -14,6 +16,14 @@ import scala.util.Random;
 
 public class GravelEvent {
 
+    public static boolean update(Entity aPlayer) {
+        if (aPlayer instanceof EntityPlayer
+                && !aPlayer.worldObj.isRemote
+                && ((EntityPlayer) aPlayer).openContainer != null)
+            ((EntityPlayer) aPlayer).openContainer.detectAndSendChanges();
+        return true;
+    }
+
     @SubscribeEvent
     public void onInteract(PlayerInteractEvent event) {
         if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK && !event.world.isRemote) {
@@ -21,10 +31,13 @@ public class GravelEvent {
                 MovingObjectPosition pos = Minecraft.getMinecraft().objectMouseOver;
                 Block b = event.entityPlayer.worldObj.getBlock(pos.blockX, pos.blockY, pos.blockZ);
                 if (Item.getItemFromBlock(b) == Item.getItemFromBlock(Blocks.gravel)) {
+                    event.world.playSoundAtEntity(event.entityPlayer, "dig.gravel", 0.45F, 1.15F);
                     if (new Random().nextInt(10) == 1) {
                         event.entityPlayer.worldObj.setBlock(pos.blockX, pos.blockY, pos.blockZ, Blocks.air, 0, 3);
                         ItemStack flint = new ItemStack(Items.flint);
-                        if (!event.entityPlayer.inventory.addItemStackToInventory(flint)) {
+                        if (event.entityPlayer.inventory.addItemStackToInventory(flint)) {
+                            update(event.entityPlayer);
+                        } else {
                             EntityItem item = new EntityItem(
                                     event.entityPlayer.worldObj,
                                     pos.blockX,
