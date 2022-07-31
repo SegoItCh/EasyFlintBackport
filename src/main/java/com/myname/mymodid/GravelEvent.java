8e64ab93d1465797264c6b58725c6959ecf579f4
Jan Grigat <j.grigat+github@cosmos-ink.net>
2022-07-31 07:28:49 +0000

    place flint to inv if there is space

--- src/main/java/com/myname/mymodid/GravelEvent.java
+++ src/main/java/com/myname/mymodid/GravelEvent.java
@@ -16,21 +16,23 @@ public class GravelEvent {
 
     @SubscribeEvent
     public void onInteract(PlayerInteractEvent event) {
-        if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
-
+        if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK && !event.world.isRemote) {
             if (Minecraft.getMinecraft().thePlayer.inventory.getCurrentItem() == null) {
                 MovingObjectPosition pos = Minecraft.getMinecraft().objectMouseOver;
                 Block b = event.entityPlayer.worldObj.getBlock(pos.blockX, pos.blockY, pos.blockZ);
                 if (Item.getItemFromBlock(b) == Item.getItemFromBlock(Blocks.gravel)) {
                     if (new Random().nextInt(10) == 1) {
-                        event.entityPlayer.worldObj.setBlock(pos.blockX, pos.blockY, pos.blockZ, Blocks.air,0,3);
-                        EntityItem item = new EntityItem(
-                                event.entityPlayer.worldObj,
-                                pos.blockX,
-                                pos.blockY,
-                                pos.blockZ,
-                                new ItemStack(Items.flint));
-                        event.entityPlayer.worldObj.spawnEntityInWorld(item);
+                        event.entityPlayer.worldObj.setBlock(pos.blockX, pos.blockY, pos.blockZ, Blocks.air, 0, 3);
+                        ItemStack flint = new ItemStack(Items.flint);
+                        if (!event.entityPlayer.inventory.addItemStackToInventory(flint)) {
+                            EntityItem item = new EntityItem(
+                                    event.entityPlayer.worldObj,
+                                    pos.blockX,
+                                    pos.blockY,
+                                    pos.blockZ,
+                                    new ItemStack(Items.flint));
+                            event.entityPlayer.worldObj.spawnEntityInWorld(item);
+                        }
                     }
                 }
             }
